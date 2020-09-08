7c29a44e58925173f3a6424292aa78e64f2dd9b0
TheClassyPenguin <salvador19marti@gmail.com>
2020-09-08 21:34:12 +0200

    Package Building, description added

--- lab3/pkg.R
+++ lab3/pkg.R
@@ -1,13 +1,13 @@
 ################################################################################
 #
-name = "Salvador Martí Román & Eleftheria LongSurname"
+name = "Salvador Mart? Rom?n & Eleftheria LongSurname"
 liuid = "salma742 & "
 #
 # 732A94 Advanced R Programming
 # Computer lab 3
 #
 lab_path = "https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab3.yml"
-
+package.skeleton(name="lab3")
 #Seminar date: 10 September 13:15 (A1, Zoom)
 # Dealine: 15 September 23:59
 ################################################################################
