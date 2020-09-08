b8960400a1603dbbf2ae979ba79dad5e4a7713b8
Eleftheria94 <e.hatzitheodoridi@gmail.com>
2020-09-08 21:54:14 +0200

    Corrected typos

--- lab3/pkg.R
+++ lab3/pkg.R
@@ -1,7 +1,7 @@
 ################################################################################
 #
-name = "Salvador Mart? Rom?n & Eleftheria LongSurname"
-liuid = "salma742 & "
+name = "Salvador Marti Roman & Eleftheria Chatzitheodoridou"
+liuid = "salma742 & elech646"
 #
 # 732A94 Advanced R Programming
 # Computer lab 3
@@ -9,7 +9,7 @@ liuid = "salma742 & "
 lab_path = "https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab3.yml"
 package.skeleton(name="lab3")
 #Seminar date: 10 September 13:15 (A1, Zoom)
-# Dealine: 15 September 23:59
+# Deadline: 15 September 23:59
 ################################################################################
 
 
