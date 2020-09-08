8cb159ff8055cd55d7703a8977a578280738cab0
TheClassyPenguin <salvador19marti@gmail.com>
2020-09-08 21:04:06 +0200

    Euclidian function finished Checks passed

---
+++ pkg.R
@@ -0,0 +1,40 @@
+################################################################################
+#
+name = "Salvador Martí Román & Eleftheria LongSurname"
+liuid = "salma742 & "
+#
+# 732A94 Advanced R Programming
+# Computer lab 3
+#
+lab_path = "https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab3.yml"
+
+#Seminar date: 10 September 13:15 (A1, Zoom)
+# Dealine: 15 September 23:59
+################################################################################
+
+
+
+#Imports
+library(markmyassignment)
+set_assignment(lab_path)
+
+
+#########
+# 1.1.1 #
+#########
+
+euclidian = function(a,b){
+  while(b!=0){
+    t = b
+    b = a %% b
+    a = t
+  }
+  return(a)
+}
+
+
+#########
+# 1.1.2 #
+#########
+
+dijkstra = function()
