e385bb0df34100b1619f0870de47bc90a4000204
TheClassyPenguin <salvador19marti@gmail.com>
2020-09-10 10:43:19 +0200

    Included Roxygen2 and added .travis.yml file

---
+++ lab3/R/euclidian.R
@@ -0,0 +1,42 @@
+################################################################################
+#
+#name = "Salvador Marti Roman & Eleftheria Chatzitheodoridou"
+#liuid = "salma742 & elech646"
+#
+# 732A94 Advanced R Programming
+# Computer lab 3
+#
+#lab_path = "https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab3.yml"
+#package.skeleton(name="lab3")
+#Seminar date: 10 September 13:15 (A1, Zoom)
+# Deadline: 15 September 23:59
+################################################################################
+
+#########
+# 1.1.1 #
+#########
+
+#' \code{euclidian} returns the lowest common denominator between two numbers.
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The lowest common denominator of \code{x} and \code{y}.
+#' @examples
+#' euclidian(1, 1)
+#' euclidian(10, 1)
+#' @export
+euclidian = function(a,b){
+  while(b!=0){
+    t = b
+    b = a %% b
+    a = t
+  }
+  return(a)
+}
+
+
+#########
+# 1.1.2 #
+#########
+
+#dijkstra = function()
